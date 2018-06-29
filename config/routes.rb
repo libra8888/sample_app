34ab5b3d33d8488ae92cc17e69e2f5dca738965f
libra8888 <ec2-user@ip-172-31-8-141.us-east-2.compute.internal>
2018-06-29 15:23:50 +0000

    Finish user destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     :registrations => "registrations"
   }
   
-  resources :users, only: [:index,:show]
+  resources :users, only: [:index, :show, :destroy]
   
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
