7616f944c5d8d17cd237ca36ee1175d8547523bf
libra8888 <ec2-user@ip-172-31-8-141.us-east-2.compute.internal>
2018-07-08 13:22:23 +0000

    Finish user follow / unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
     :registrations => "registrations"
   }
   
-  resources :users, only: [:index, :show, :destroy]
+  resources :users, only: [:show, :index, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
